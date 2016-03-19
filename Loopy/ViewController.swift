f786479029de6852ba23958319c9e908da84cf4c
Agree <agree.ahmed@gmail.com>
2016-03-19 04:41:40 -0400

    first commit

--- Loopy/ViewController.swift
+++ Loopy/ViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import MobileCoreServices
+import Kingfisher
+import Regift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var gifWindow: UIImageView!
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]){
+            let videoURL = info[UIImagePickerControllerMediaURL] as! NSURL!
+            let frameCount = 16
+            let delayTime  = Float(0.2)
+            let loopCount  = 0    // 0 means loop forever
+            let regift = Regift(sourceFileURL: videoURL, frameCount: frameCount, delayTime: delayTime, loopCount: loopCount)
+            let gif_url = regift.createGif()
+            gifWindow.kf_setImageWithURL(gif_url!)
+            // gotta load that into the image view
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController){
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    @IBAction func selectVideoPressed(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum) {
+            imagePicker.delegate = self
+            imagePicker.mediaTypes = [kUTTypeMovie as String!]
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
 
-
+    }
 }
 
